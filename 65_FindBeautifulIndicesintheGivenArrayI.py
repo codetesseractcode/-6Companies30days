d5d0a15c5759176099474a84dda43bfd5d66b669
Ayusman Pradhan <pradhanayusman22k@gmail.com>
2025-01-24 20:58:33 +0530

    Completed 65th Problem

--- 65_FindBeautifulIndicesintheGivenArrayI.py
+++ 65_FindBeautifulIndicesintheGivenArrayI.py
@@ -1,21 +1,32 @@
 class Solution:
-    def wordCount(self, startWords: List[str], targetWords: List[str]) -> int:
-        sw = {}
-        
-        for ss in startWords:
-            sw[ss] = Counter(ss)
-            
-        
-        S = set()
-        
-        for target in targetWords:
-            counter_t = Counter(target)
-            
-            for ss in sw:
-                if len(target) == len(ss) + 1 and len(counter_t.keys()) == len(sw[ss].keys()) + 1:
-                    s = sum([val for key, val in (sw[ss] & counter_t).items()])
-                    if s == len(ss):
-                        S.add(target)
-                        break
+    def beautifulIndices(self, s: str, a: str, b: str, k: int) -> List[int]:
+        len_a = len(a)
+        len_b = len(b)
+        idx_a = []
+        idx_b = []
 
-        return len(S)
+        for i in range(0, len(s) - len_a + 1):
+            if s[i:i+len_a] == a:
+                idx_a.append(i)
+
+        for i in range(0, len(s) - len_b + 1):
+            if s[i:i+len_b] == b:
+                idx_b.append(i)
+
+        res = []
+        if not len(idx_a) or not len(idx_b):
+            return res
+        i = 0
+        j = 0
+
+        while i < len(idx_a) and j < len(idx_b):
+            if abs(idx_a[i] - idx_b[j]) <= k:
+                res.append(idx_a[i])
+                i += 1
+            elif idx_a[i] > idx_b[j]:
+                j += 1
+            else:
+                i += 1
+            
+        return res
+        
